45919aea1486274d6e8aaeacd28096a57073328c
VLAD <19872650+dreezyduh@users.noreply.github.com>
2024-02-08 12:38:32 +0200

    Add rainbow color button

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # etch-a-sketch
 Resizable and drawable grid made in javaScript, css and html
+Live code can be accessed at : https://dreezyduh.github.io/etch-a-sketch/
--- script.js
+++ script.js
@@ -9,16 +9,22 @@ resetButton.addEventListener("click", drawGrid);
 function drawGrid() {
     container.textContent = "";
     let number = prompt(`Insert a number \nmax is 100 \n( example : 100 = 100 rows and columns )`)
-    if ((number > 0) && (number <=100)) {
-        for ( j = 1; j <= number; j++ ) {
+    if ((number > 0) && (number <= 100)) {
+        for (j = 1; j <= number; j++) {
             const j = document.createElement("div");
-            j.setAttribute ("class", "gridItemColumn");
+            j.setAttribute("class", "gridItemColumn");
             container.appendChild(j)
-            for ( i = 1; i <= number; i++ ) {
+            for (i = 1; i <= number; i++) {
                 const i = document.createElement("div");
-                i.setAttribute ("class", "gridItemHorizontal");
+                i.setAttribute("class", "gridItemHorizontal");
                 i.addEventListener("mouseenter", function (e) {
-                    e.target.style.backgroundColor = chosenColor;
+                    switch (chosenColor) {
+                        case "rainbow":
+                            e.target.style.backgroundColor = rainbow()
+                            break;
+                        default:
+                            e.target.style.backgroundColor = chosenColor;
+                    }
                 });
                 j.appendChild(i)
             }
@@ -26,24 +32,34 @@ function drawGrid() {
 
     } else {
         alert(`Invalid number, try writing one between 1 and 64`);
-    }   
+    }
 }
 
 let chosenColor = "white";
-const buttonColors = [ "red", "blue", "green", "yellow", "pink", "purple", "turquoise", "white", "black", "lime", "lavender", "DarkSalmon","LightCoral", "PeachPuff", "orange"]
+const buttonColors = ["red", "blue", "green", "yellow", "pink", "purple", "turquoise", "white", "black", "lime", "lavender", "DarkSalmon", "LightCoral", "PeachPuff", "orange", "rainbow"]
 
 const buttonContainer = document.createElement("div")
 buttonContainer.setAttribute("class", "buttonContainer")
 header.appendChild(buttonContainer);
 
 let counter = 0;
-for ( i = 0; i < buttonColors.length; i++) {
+for (i = 0; i < buttonColors.length; i++) {
     const i = document.createElement("button")
     i.textContent = buttonColors[counter];
-    i.addEventListener("click", function (){
+    i.addEventListener("click", function () {
         chosenColor = i.textContent;
     })
     counter += 1;
     buttonContainer.appendChild(i);
 }
 
+function random(number) {
+    return Math.floor(Math.random() * number)
+}
+
+function rainbow() {
+    let r = random(255);
+    let g = random(255);
+    let b = random(255);
+    return `rgb(${r}, ${g}, ${b})`
+}
